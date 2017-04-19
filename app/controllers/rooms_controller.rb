6d50bd8f0dc6c0fa56aef439e23a061936de95fb
Javier Vargas <javier1615@gmail.com>
2017-04-19 12:23:51 -0500

    adding-edit-and-delete-room-function

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -7,6 +7,10 @@ class RoomsController < ApplicationController
     @room = Room.new
   end
 
+  def edit
+    @room = Room.find(params[:id])
+  end
+
   def create
     @room = Room.new(room_params)
     if @room.save
@@ -14,6 +18,24 @@ class RoomsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def update
+    @room = Room.find(params[:id])
+
+    if @room.update(room_params)
+      redirect_to rooms_path
+    else
+      render 'edit'
+    end
+  end
+
+
+  def destroy
+    @room = Room.find(params[:id])
+    @room.destroy
+
+    redirect_to rooms_path
   end
 
   protected
