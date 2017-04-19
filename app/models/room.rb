013c38e323799f3cc7064da4245bde4eb88db4bd
Javier Vargas <javier1615@gmail.com>
2017-04-19 08:47:33 -0500

    adding-validation-to-rooms-model

--- app/models/room.rb
+++ app/models/room.rb
@@ -13,4 +13,17 @@
 #
 
 class Room < ActiveRecord::Base
+	validates :title, :description, :beds, :guests, :image_url, presence: true
+	validates :description, length: { maximum: 400 }
+	validate :field_even?
+
+
+	def field_even?
+		case 
+		when guests != nil && !guests.even?
+			errors.add(:guests, "is not even")
+		when beds != nil && !beds.even?
+			errors.add(:beds, "is not even")
+		end
+	end
 end
